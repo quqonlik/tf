c15938a2109e6521a9a9cd0ce37e023f2fd13de6
quqonlik <quqonlik@gmail.com>
2022-07-15 14:58:05 -0500

    Updatefile

---
+++ createInstance.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    version = "4.20.0"
+    access_key = "AKIA4I3L325NNQ5HIZZE"
+    secret_key = "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"
+    region     = "us-east-2"
+}
+resource "aws_instance" "MyFirstInstnace" {
+  count         = 3
+  ami           = "ami-05803413c51f242b7"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "demoinstnce-${count.index}"
+  }
+}

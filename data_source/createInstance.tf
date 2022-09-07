a929f50916000a55eae0d6c68c8641338c4168de
quqonlik <quqonlik@gmail.com>
2022-09-06 22:50:11 -0500

    chaning zone to 1

--- data_source/createInstance.tf
+++ data_source/createInstance.tf
@@ -19,7 +19,7 @@ data "aws_ami" "latest-ubuntu" {
 resource "aws_instance" "MyFirstInstnace" {
   ami           = data.aws_ami.latest-ubuntu.id
   instance_type = "t2.micro"
-  availability_zone = data.aws_availability_zones.avilable.names[2]
+  availability_zone = data.aws_availability_zones.avilable.names[1]
 
   provisioner "local-exec" {
     command = "echo ${aws_instance.MyFirstInstnace.private_ip} >> my_private_ips.txt"

103a3d8aacd5b6cb5b46d02a6f43c6738c2e9be3
quqonlik <quqonlik@gmail.com>
2023-01-14 13:44:56 -0600

    new changes

--- basics/createInstance.tf
+++ basics/createInstance.tf
@@ -1,10 +1,10 @@
-provider "aws" {
-    access_key = "AKIA4I3L325NNQ5HIZZE"
-    secret_key = "SECRETKEY"
-    region     = "us-east-2"
-}
 resource "aws_instance" "MyFirstInstnace" {
   count         = 3
   ami           = "ami-05803413c51f242b7"
   instance_type = "t2.micro"
+ 
+ tags{
+   Name = "threedemoinstances"
+ }
+ 
   }

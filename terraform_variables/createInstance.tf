569f924c64d85f7d972a7ca2f0c1898d02034bd4
quqonlik <quqonlik@gmail.com>
2023-01-15 17:18:54 -0600

    testing git ignore

--- terraform_variables/createInstance.tf
+++ terraform_variables/createInstance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "MyFirstInstnace" {
   instance_type = "t2.micro"
 
 tags = {
-    Name = "demoinstance"
+    Name = "linuxmachine"
  }
 
  security_groups = "${var.Security_Group}"

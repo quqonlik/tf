d9b8b2d5ac56cb285c8a22b9ce5744c5d0d353fd
quqonlik <quqonlik@gmail.com>
2023-01-15 18:09:08 -0600

    update

--- terraform_variables/variables.tf
+++ terraform_variables/variables.tf
@@ -5,9 +5,10 @@ variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
 default = "us-east-2"
 }
-variable "Security_Group"{
+variable "Security_Group"{ 
     type = list
     default = ["sg-10001", "sg-10002", "sg-10003"]
+    }
 
 variable "AMIS"{
     type = map

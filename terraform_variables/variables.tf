0fc0a77df0c132faa11857a12723ac463b2b00f0
quqonlik <quqonlik@gmail.com>
2023-01-15 18:07:37 -0600

    update

--- terraform_variables/variables.tf
+++ terraform_variables/variables.tf
@@ -9,7 +9,7 @@ variable "Security_Group"{
     type = list
     default = ["sg-10001", "sg-10002", "sg-10003"]
 
-variable "AMIS" {
+variable "AMIS"{
     type = map
     default = {
         us-east-1 = ami-0b0ea68c435eb488d

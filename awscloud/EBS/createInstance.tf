b79a47b895fdb483155b8d2eb48ea4fdcfdbe5ba
quqonlik <quqonlik@gmail.com>
2023-01-17 21:49:28 -0600

    update

--- awscloud/EBS/createInstance.tf
+++ awscloud/EBS/createInstance.tf
@@ -33,4 +33,5 @@ resource "aws_volume_attachment" "ebs-volume-1-attachment" {
   device_name = "/dev/xvdh"
   volume_id   = aws_ebs_volume.ebs-volume-1.id
   instance_id = aws_instance.MyFirstInstnace.id
+
 }

1fdda6f828e32ae165401efaefb96437ff7ddcc0
quqonlik <quqonlik@gmail.com>
2023-01-17 21:47:19 -0600

    fix

--- awscloud/EBS/createInstance.tf
+++ awscloud/EBS/createInstance.tf
@@ -4,17 +4,18 @@ resource "aws_key_pair" "levelup_key" {
     public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
-#Create AWS Instance
 resource "aws_instance" "MyFirstInstnace" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
-  availability_zone = "us-east-2a"
   key_name      = aws_key_pair.levelup_key.key_name
-}
+
+  vpc_security_group_ids = [aws_security_group.allow-levelup-ssh.id]
+  subnet_id = aws_subnet.levelupvpc-public-2.id
 
   tags = {
     Name = "custom_instance"
   }
+}
 
 #EBS resource Creation
 resource "aws_ebs_volume" "ebs-volume-1" {

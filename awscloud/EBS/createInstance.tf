b432d9c8bf06df03ebaec1fac2fe7c0e66856633
quqonlik <quqonlik@gmail.com>
2023-01-17 21:43:25 -0600

    fix

--- awscloud/EBS/createInstance.tf
+++ awscloud/EBS/createInstance.tf
@@ -1,18 +1,20 @@
+
 resource "aws_key_pair" "levelup_key" {
     key_name = "levelup_key"
     public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
+#Create AWS Instance
 resource "aws_instance" "MyFirstInstnace" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
+  availability_zone = "us-east-2a"
   key_name      = aws_key_pair.levelup_key.key_name
-}
-  
- 
+
   tags = {
     Name = "custom_instance"
   }
+}
 
 #EBS resource Creation
 resource "aws_ebs_volume" "ebs-volume-1" {
@@ -23,14 +25,11 @@ resource "aws_ebs_volume" "ebs-volume-1" {
   tags = {
     Name = "Secondary Volume Disk"
   }
-
 }
+
 #Atatch EBS volume with AWS Instance
 resource "aws_volume_attachment" "ebs-volume-1-attachment" {
   device_name = "/dev/xvdh"
   volume_id   = aws_ebs_volume.ebs-volume-1.id
   instance_id = aws_instance.MyFirstInstnace.id
 }
- 
-
-

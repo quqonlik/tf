d8d9cf0123f0904b7a5ba3f25012c794e54cd6cc
quqonlik <quqonlik@gmail.com>
2023-01-23 15:00:16 -0600

    update

--- IAM/Roles/s3bucket.tf
+++ IAM/Roles/s3bucket.tf
@@ -1,11 +1,11 @@
 #Create AWS S3 Bucket
 
 resource "aws_s3_bucket" "levelup_s3bucket" {
-  bucket = "levelup_bucket_141"
+  bucket = "levelup-bucket-141"
   acl    = "private"
 
   tags = {
-    Name = "levelup_bucket_141"
+    Name = "levelup-bucket-141"
   }
 }
 
